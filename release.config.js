e600fbc8c03681e636ae86908f6af7eefeb25f8e
Mat Merrigan <matm@merriganenergy.com>
2024-01-01 17:58:01 -0600

    ci: setup a dry run of semantic-release
    https://semantic-release.gitbook.io/semantic-release/usage/configuration
    https://semantic-release.gitbook.io/semantic-release/usage/plugins
    https://github.com/semantic-release/commit-analyzer
    
    https://github.com/KengoTODA/gradle-semantic-release-plugin
    https://github.com/semantic-release/exec
    https://github.com/semantic-release/git

---
+++ release.config.js
@@ -0,0 +1,24 @@
+export const config = {
+  tagFormat: "${version}",
+  plugins: [
+    // default plugins
+    ["@semantic-release/commit-analyzer", {
+      preset: "conventionalcommits"
+    }],
+    ["@semantic-release/release-notes-generator", {
+      preset: "conventionalcommits"
+    }],
+    ["@semantic-release/github", {
+      addReleases: "bottom",
+    }],
+    // additional plugins
+    "@semantic-release/changelog",
+    // TODO: setup a template file for README.md
+    "@semantic-release/exec",
+    ["@semantic-release/git", {
+      assets: [ "./*.md", "./**/*/.md", "gradle.properties" ],
+    }],
+    "gradle-semantic-release-plugin",
+  ],
+  dryRun: process.env.PUBLISH_DRY_RUN == true,
+}
